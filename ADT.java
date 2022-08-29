9cd2e82cb616dcae2501c0ec14ad8000d3e3ed7b
Vania <98841120+Enanacc@users.noreply.github.com>
2022-08-28 21:38:32 -0500

    Create ADT.java

---
+++ ADT.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Arreglo;
+
+import Metodos.Magic;
+
+/**
+*
+* @author Vania
+*/
+public class ADT {
+  
+  public static void main(String[] args) {
+     Magic 0 = new Magic();
+     Magic<String> 0k = new Magic<String>():
+  
+     0k.setElemento(0, "Hola Mundo");
+  
+     0k.getElemento(0);
+  
+  
+     System.out.printl(0k);
+   }
+
+  
+}
